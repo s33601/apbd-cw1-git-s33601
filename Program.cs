e5f756bdba02e652df54d0bc81dfef1541228b16
s33601 <s33601@pjwstk.edu.pl>
2026-03-16 16:55:22 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
 Console.WriteLine("Wprowadz liczby oddzielone przecinkami: ");
 string input = Console.ReadLine();
-if (string.IsNullOrEmpty(input)) {Console.WriteLine("Brak danych.");return;}
+if (string.IsNullOrEmpty(input)) {
+  Console.WriteLine("Brak danych.");
+  return;
+}
